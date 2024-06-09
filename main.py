c9ef09e588c9192b008a5e6ef94aa99e7ae1548f
kamisaberi <kamisaberi@gmail.com>
2024-06-09 13:00:51 +0330

    bugs fixed

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
+import threading
+
 import adidas as ad
 import sys
 import time
 
 if __name__ == "__main__":
     ad.AdidasThread.Settings.load_settings()
-    # while True:
-    #     pass
-    #
-    # i = 1
-    # while True:
-    thread1 = ad.AdidasThread(1, ad.TYPES.GET_ITEMS_LIST)
-    thread1.start()
-    thread1.join(0.1)
-
-    # thread1 = ad.AdidasThread(2, ad.TYPES.GET_ITEMS_LIST)
-    # thread1.start()
-    # thread1.join(0.2)
-    # time.sleep(10)
-
-    # thread2 = ad.AdidasThread(2, ad.TYPES.GET_REVIEWS)
-    # thread2.start()
-    # thread2.join()
-    # thread.join()
-    # i += 1
+    threads: [threading.Thread] = []
+    while True:
+        thread1 = ad.AdidasThread(threading.active_count(), ad.TYPES.GET_ITEMS_LIST)
+        thread1.start()
+        thread1.join(0.1)
 
     ad.AdidasThread.Settings.save_settings()
