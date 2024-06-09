3c96ad84fb908193404df63e07348c7991f7227b
kamisaberi <kamisaberi@gmail.com>
2024-06-09 13:20:54 +0330

    bugs fixed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import threading
+from typing import Any
 
 import adidas as ad
 import sys
@@ -8,8 +9,15 @@ if __name__ == "__main__":
     ad.AdidasThread.Settings.load_settings()
     threads: [threading.Thread] = []
     while True:
-        thread1 = ad.AdidasThread(threading.active_count(), ad.TYPES.GET_ITEMS_LIST)
-        thread1.start()
-        thread1.join(0.1)
+        if len(ad.AdidasThread.items) < ad.AdidasThread.Settings.items_count:
+            thread = ad.AdidasThread(len(threads) + 1, ad.TYPES.GET_ITEMS_LIST)
+            thread.start()
+            thread.join(0.1)
+            threads.append(thread)
+        if len(ad.AdidasThread.model_product_objects) > 0:
+            thread = ad.AdidasThread(len(threads) + 1, ad.TYPES.GET_REVIEWS)
+            thread.start()
+            thread.join(0.1)
+            threads.append(thread)
 
     ad.AdidasThread.Settings.save_settings()
