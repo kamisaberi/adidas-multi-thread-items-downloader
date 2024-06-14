0e8065eca1e213f349e2fc617fcf50452464365e
= <=>
2024-06-15 01:30:04 +0330

    static properties converted to namedtuple

--- crap.py
+++ crap.py
@@ -4,3 +4,8 @@ def print_progress_bar(progress, total):
     percent = 100 * (progress / float(total))
     bar = '█' * int(percent) + ('░' * (100 - int(percent)))
     print(f"\r|{bar}| {percent:.2f}%", end="\r")
+
+
+
+
+
