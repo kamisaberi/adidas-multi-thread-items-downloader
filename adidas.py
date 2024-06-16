f4d7bb3ee80f76b0c594043804eebb68d07d9bdb
kamisaberi <kamisaberi@gmail.com>
2024-06-16 13:43:43 +0330

    code refined

--- adidas.py
+++ adidas.py
@@ -49,6 +49,7 @@ class AdidasThread(threading.Thread):
             items_threads_count:
             reviews_threads_count:
     """
+
     @staticmethod
     def load_templates():
         with open("prefs/templates.json") as file:
@@ -59,30 +60,19 @@ class AdidasThread(threading.Thread):
                 named_tuples.append(nt)
             return named_tuples
 
-    # STATIC PROPERTIES
-    events: namedtuple = (namedtuple("events", ["should_load_settings", "should_update_settings"])
-                          (threading.Event(), threading.Event()))
-    events.should_load_settings.set()
-    events.should_update_settings.clear()
-    # urls: namedtuple = (namedtuple("urls", ["items", "reviews"])
-    #                     ("https://www.adidas.at/api/plp/content-engine/search?",
-    #                      "https://www.adidas.at/api/models/{model_id}/reviews?bazaarVoiceLocale=de_AT&feature&includeLocales=de%2A&limit={limit}&offset={offset}&sort=newest"))
-    #
-    # paths: namedtuple = (namedtuple("paths", ["settings_file_path", "product_file_name_prefix", "product_files_path"])
-    #                      ("prefs/settings.json", "pr-", "files"))
-    # templates: namedtuple = (namedtuple("templates", ["params", "headers"])
-    #                          ({'query': 'all',
-    #                            "start": 0,
-    #                            "sort": "newest-to-oldest"
-    #                            },
-    #                           {'authority': 'www.adidas.at',
-    #                            'accept': '*/*',
-    #                            'accept-language': 'en-US,en;q=0.9,fa;q=0.8',
-    #                            'content-type': 'application/json',
-    #                            'user-agent': 'PostmanRuntime/7.35.0'}
-    #                           ))
+    @staticmethod
+    def initialize_events():
+        should_load_settings = threading.Event()
+        should_load_settings.set()
+        should_update_settings = threading.Event()
+        should_update_settings.clear()
+        return (namedtuple("events", ["should_load_settings", "should_update_settings"])
+                (should_load_settings, should_update_settings))
 
+    # STATIC PROPERTIES
+    events: namedtuple = initialize_events()
     urls, paths, templates = load_templates()
+
     items: list[dict] = list()
     model_product_objects: list[tuple[str, str]] = list()
     next_start_point: int = 0
