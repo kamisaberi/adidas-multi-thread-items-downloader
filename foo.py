e550324394feceb2f40912afbb59ed5731870610
= <=>
2024-06-18 20:27:26 +0330

    fuck python

--- foo.py
+++ foo.py
@@ -5,6 +5,5 @@ class Foo(object):
 
     def __getattr__(self, name):
         # return super(Foo, self).__getattribute__(name)
-
         # print(name)
         return self.__dict__.setdefault(name)
--- foo.pyi
+++ foo.pyi
@@ -2,3 +2,6 @@ from typing import Any
 class Foo(object):
     def __init__(self , first_name : str = "" ,last_name:str = "" , age : int = 0 )->None:...
     def __getattr__(self, name: str) -> Any | None: ... # incomplete
+
+    @property
+    def first_name(self)->str:...
